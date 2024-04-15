71aed61a41ff70b4d0feab742021bedb4d87b274
nettertech <166582562+nettertech@users.noreply.github.com>
2024-04-15 02:42:06 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,29 @@
 var dialog = document.getElementsByClassName("dialog")[0];
+var dialog2 = document.getElementsByClassName("dialog2")[0];
 var variableBorder = 1;
 var changeBorder = setInterval(change, 300);
+var next = 0;
+dialog2.style.border = "none";
 
 function change() {
-    if (variableBorder == 1) {
+    if (variableBorder == 1 && next == 0) {
         dialog.style.border = "none";
+        dialog2.style.border = "none";
         variableBorder = 2;
-    } else if (variableBorder == 2) {
+    }
+    else if (variableBorder == 2 && next == 0) {
         dialog.style.borderRight = "10px solid white";
+        dialog2.style.border = "none";
+        variableBorder = 1;
+    }
+    else if (variableBorder == 1 && next == 1) {
+        dialog.style.border = "none";
+        dialog2.style.border = "none";
+        variableBorder = 2;
+    }
+    else if (variableBorder == 2 && next == 1) {
+        dialog.style.border = "none";
+        dialog2.style.borderRight = "10px solid white";
         variableBorder = 1;
     }
 }
@@ -35,13 +51,17 @@ var namber = 0;
 var timer;
 var texts;
 
+
 type();
 
 function type() {
-    dialog.style.fontsize = "50px"
+    dialog.style.fontsize = "50px";
+    dialog2.style.fontsize = "50px";
+    dialog.style.marginLeft = "0px";
     dialogs["value" + namber]();
 
     dialog.innerHTML = "";
+    dialog2.innerHTML = "";
     var Letters = [];
 
     for (var i = 0; i < texts.length; i++) {
@@ -52,7 +72,7 @@ function type() {
     writes = setInterval(write, timer);
 
     positionLetter = 0;
-
+    var b = 0;
     function write() {
         if (positionLetter == Letters.length) {
             clearInterval(writes);
@@ -60,12 +80,30 @@ function type() {
             if (namber == Object.keys(dialogs).length) {
                 clearInterval(writes);
                 namber = 0;
-                //positionLetter = 0;                
+                positionLetter = 0;                
             }
             setTimeout(type, 1500);
             return;
         }
-        dialog.innerHTML += Letters[positionLetter];
-        positionLetter++;
+        //dialog.innerHTML += Letters[positionLetter];
+        //positionLetter++;
+        if(namber == 2 && positionLetter <= 6){
+            dialog2.innerHTML += Letters[positionLetter];
+            b += 28;
+            dialog.style.marginLeft = b+"px";
+            positionLetter++;
+            next = 1;
+        }
+        else if(namber == 2 && positionLetter > 6){
+            dialog2.style.border = "none";
+            next = 0;
+            dialog.innerHTML += Letters[positionLetter];
+            positionLetter++;            
+        }
+        else{
+            dialog.innerHTML += Letters[positionLetter];
+            positionLetter++;
+            //dialog.style.marginLeft = "0px";
+        }
     }
 }
